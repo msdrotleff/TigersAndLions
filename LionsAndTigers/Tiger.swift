945943b0f3979d42dfaf35e7976997f434b05f41
maritzadrotleff <maritza.s.garcia@gmail.com>
2014-11-03 14:37:44 -0800

    First Rev

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Maritza Drotleff on 10/31/14.
+//  Copyright (c) 2014 Maritza Drotleff. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = " "
+    var image = UIImage (named: "")
+}
